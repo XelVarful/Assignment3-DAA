963573a53f26e839f2b10060a0355bb83d19dc39
XelVarful <adilbekarman4@gmail.com>
2025-10-26 14:06:40 +0500

    Add Graph class to store nodes and edges

--- src/main/java/model/Graph.java
+++ src/main/java/model/Graph.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Graph {
+    public int id;
+    public List<String> nodes;
+    public List<Edge> edges;
+
+    public Graph() {
+        nodes = new ArrayList<>();
+        edges = new ArrayList<>();
+    }
 }
